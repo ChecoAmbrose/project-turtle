e0b11b5c9bf095707dc816f755afd117d834a22a
checolap <elite_shergio@hotmail.es>
2021-01-04 16:33:11 -0600

    Página arreglada para el usuario

--- prose-form.php
+++ prose-form.php
@@ -11,58 +11,47 @@
    $tema="Contacto desde el Sitio Web";
    $mensaje = "
                        <table border='0' cellspacing='2' cellpadding='2'>
+
                           <tr>
                             <td width='20%' align='center' bgcolor='#FFFFCC'><strong>Nombre:</strong></td>
                             <td width='80%' align='left'>".$_POST["Nombre"]."</td>
                           </tr>
+                          
+                          <tr>
+                          <td align='center' bgcolor='#FFFFCC'><strong>Telefono:</strong></td>
+                          <td align='left'>".$_POST["Telefono"]."</td>
+                        </tr>
+
+                        <tr>
+                        <td align='center' bgcolor='#FFFFCC'><strong>Correo:</strong></td>
+                        <td align='left'>".$_POST["Correo"]."</td>
+                        </tr>
+
                           <tr>
                             <td align='center' bgcolor='#FFFFCC'><strong>Empresa:</strong></td>
                             <td align='left'>".$_POST["Empresa"]."</td>
                           </tr>
-                           <tr>
-                            <td width='20%' align='center' bgcolor='#FFFFCC'><strong>Cargo</strong></td>
-                            <td width='80%' align='left'>".$_POST["Cargo"]."</td>
-                          </tr>
-                          <tr>
-                            <td align='center' bgcolor='#FFFFCC'><strong>Dirección:</strong></td>
-                            <td align='left'>".$_POST["Dirección"]."</td>
-                          </tr>
-                            <tr>
-                            <td align='center' bgcolor='#FFFFCC'><strong>Telefono:</strong></td>
-                            <td align='left'>".$_POST["Telefono"]."</td>
-                          </tr>
-                            <tr>
-                            <td align='center' bgcolor='#FFFFCC'><strong>Correo:</strong></td>
-                            <td align='left'>".$_POST["Correo"]."</td>
-                          </tr>
+
                             <tr>
                             <td align='center' bgcolor='#FFFFCC'><strong>Comentarios:</strong></td>
                             <td align='left'>".$_POST["Comentarios"]."</td>
                           </tr>
+
                             <tr>
                             <td align='center' bgcolor='#FFFFCC'><strong>Instrumento:</strong></td>
                             <td align='left'>".$_POST["Instrumento"]."</td>
                           </tr>
+
                             <tr>
-                            <td align='center' bgcolor='#FFFFCC'><strong>Marca del instrumento:</strong></td>
-                            <td align='left'>".$_POST["Marca"]."</td>
+                            <td align='center' bgcolor='#FFFFCC'><strong>Descripcion del servicio:</strong></td>
+                            <td align='left'>".$_POST["Descripcion"]."</td>
                           </tr>
-                              <tr>
-                            <td align='center' bgcolor='#FFFFCC'><strong>Modelo:</strong></td>
-                            <td align='left'>".$_POST["Modelo"]."</td>
-                          </tr>
-                                <tr>
-                            <td align='center' bgcolor='#FFFFCC'><strong>Medida:</strong></td>
-                            <td align='left'>".$_POST["Intervalo"]."</td>
-                          </tr>
-                              <tr>
-                            <td align='center' bgcolor='#FFFFCC'><strong>Puntos a calibrar:</strong></td>
-                            <td align='left'>".$_POST["Puntos"]."</td>
-                          </tr>
+
                         </table>
                         Enviado: $fecha a las $hora\n\n"; 
           
     mail ($to, $tema, $mensaje, $headers);
-    echo "Mensaje enviado!!!<br /><br /> <a href='6-contactanos.php'>Volver</a>";
+    echo "<script>alert('Correo Enviado.')</script>";
+    echo "<script> setTimeout(\location.href='index.html'\",1000</script>";
   
 ?>
